36e5fc9cb0785c3c59fa35224f814d8da1f9d5d1
Silvan <t3102115@gmail.com>
2026-01-22 20:42:40 +0100

    Update sw.js

--- irgendwiealles/sw.js
+++ irgendwiealles/sw.js
@@ -1,12 +1,6 @@
 self.addEventListener("message", event => {
-    const { name, notifyTime } = event.data;
-    const delay = notifyTime - Date.now();
-
-    if (delay <= 0) return;
-
-    setTimeout(() => {
-        self.registration.showNotification("Minecraft Erinnerung", {
-            body: name + " ist in 10 Minuten dran"
-        });
-    }, delay);
+    const { name, text } = event.data;
+    self.registration.showNotification("Minecraft Erinnerung", {
+        body: `${name} ${text}`
+    });
 });
