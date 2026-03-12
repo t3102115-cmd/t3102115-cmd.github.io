07f2e8b9061e240aa89f8eed8a12eaa9a9b083e1
Silvan <t3102115@gmail.com>
2026-03-12 20:38:46 +0100

    Delete cloud/preview.js

--- cloud/preview.js
+++
@@ -1,6 +0,0 @@
-async function previewFile(name) {
-  const r = await fetch(AUTH + "/preview/" + name + "?token=" + sessionToken);
-  const blob = await r.blob();
-  const url = URL.createObjectURL(blob);
-  window.open(url);
-}
